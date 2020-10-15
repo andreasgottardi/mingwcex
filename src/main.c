a755e02fc5923dc66bf8df8706685988833f8c0a
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-10-15 06:38:19 +0200

    Gui example added.

--- src/main.c
+++ src/main.c
@@ -2,11 +2,56 @@
 #include <stdlib.h>
 #include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	printf("Hello World!\n");
-	int i = 4711;
-	printf("Value of i: %d.\n", i);
-	return 0;
+
+	MSG msg;
+	HWND hwnd;
+	WNDCLASSW wc;
+	
+	MessageBoxW(NULL, L"First Program", L"First", MB_OK);
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.lpszClassName = L"Window";
+	wc.hInstance = hInstance;
+	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+	wc.lpszMenuName = NULL;
+	wc.lpfnWndProc = WndProc;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+
+	RegisterClassW(&wc);
+	hwnd = CreateWindowW(wc.lpszClassName, L"Window",
+						 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+						 100, 100, 350, 250, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		DispatchMessage(&msg);
+	}
+
+	return (int) msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
+						 WPARAM wParam, LPARAM lParam)
+{
+
+	switch (msg)
+	{
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
